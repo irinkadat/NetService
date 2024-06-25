165e55bae2f025fcf66033bc6c07364ca22e7dd2
irinkadat <irina.datoshvili.1@btu.edu.ge>
2024-06-25 10:00:17 +0400

    Add network service with generics

--- Sources/NetService/NetService.swift
+++ Sources/NetService/NetService.swift
@@ -1,2 +1,57 @@
-// The Swift Programming Language
-// https://docs.swift.org/swift-book
+import Foundation
+
+enum NetworkError: Error {
+    case decodeError
+    case wrongResponse
+    case wrongStatusCode(code: Int)
+    case invalidURL
+}
+
+public class NetworkService {
+    
+    public init() {}
+    
+    public func getData<T: Decodable>(urlString: String, headers: [String: String]? = nil, completion: @escaping (Result<T, Error>) ->(Void)) {
+        guard let url = URL(string: urlString) else {
+            completion(.failure(NetworkError.invalidURL))
+            return
+        }
+        
+        var request = URLRequest(url: url)
+        headers?.forEach { key, value in
+            request.addValue(value, forHTTPHeaderField: key)
+        }
+        
+        URLSession.shared.dataTask(with: request) { data, response, error in
+            if let error = error {
+                completion(.failure(error))
+                return
+            }
+            
+            guard let response = response as? HTTPURLResponse else {
+                completion(.failure(NetworkError.wrongResponse))
+                return
+            }
+            
+            guard (200...299).contains(response.statusCode) else {
+                completion(.failure(NetworkError.wrongStatusCode(code: response.statusCode)))
+                return
+            }
+            
+            guard let data = data else {
+                completion(.failure(NetworkError.decodeError))
+                return
+            }
+            
+            do {
+                let decoder = JSONDecoder()
+                let object = try decoder.decode(T.self, from: data)
+                DispatchQueue.main.async {
+                    completion(.success(object))
+                }
+            } catch {
+                completion(.failure(NetworkError.decodeError))
+            }
+        }.resume()
+    }
+}
