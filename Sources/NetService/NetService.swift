fd2a34ae81be87ab39cb2f88c1143452c219a338
irinkadat <irina.datoshvili.1@btu.edu.ge>
2024-07-24 23:01:28 +0400

    update network service, add post data method with generics

--- Sources/NetService/NetService.swift
+++ Sources/NetService/NetService.swift
@@ -54,4 +54,57 @@ public class NetworkService {
             }
         }.resume()
     }
+    
+    public func postData<T: Decodable>(urlString: String, body: [String: Any], headers: [String: String]? = nil, completion: @escaping (Result<T, Error>) ->(Void)) {
+        guard let url = URL(string: urlString) else {
+            completion(.failure(NetworkError.invalidURL))
+            return
+        }
+        
+        var request = URLRequest(url: url)
+        request.httpMethod = "POST"
+        headers?.forEach { key, value in
+            request.addValue(value, forHTTPHeaderField: key)
+        }
+        
+        do {
+            let jsonData = try JSONSerialization.data(withJSONObject: body, options: [])
+            request.httpBody = jsonData
+        } catch {
+            completion(.failure(error))
+            return
+        }
+        
+        URLSession.shared.dataTask(with: request) { data, response, error in
+            if let error = error {
+                completion(.failure(error))
+                return
+            }
+            
+            guard let response = response as? HTTPURLResponse else {
+                completion(.failure(NetworkError.wrongResponse))
+                return
+            }
+            
+            guard (200...299).contains(response.statusCode) else {
+                completion(.failure(NetworkError.wrongStatusCode(code: response.statusCode)))
+                return
+            }
+            
+            guard let data = data else {
+                completion(.failure(NetworkError.decodeError))
+                return
+            }
+            
+            do {
+                let decoder = JSONDecoder()
+                let object = try decoder.decode(T.self, from: data)
+                DispatchQueue.main.async {
+                    completion(.success(object))
+                }
+            } catch {
+                completion(.failure(NetworkError.decodeError))
+            }
+        }.resume()
+    }
 }
